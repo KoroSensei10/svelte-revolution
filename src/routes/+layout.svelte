c1bf3e84b1be31e06da40b4d28b97fa78bb40708
Mathis <mathisjung02@gmail.com>
2024-09-19 08:32:40 +0200

    japanese + bun + fix dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,25 +6,31 @@
 	import { Toaster } from 'svelte-french-toast';
 </script>
 
-<Toaster />
-
-{#if $isLoading}
-	<p>Loading...</p>
-{:else}
-	<select bind:value={$locale} class="fixed bottom-0 right-0 rounded-tl-xl bg-black p-4 text-white">
-		{#each $locales as l, i}
-			<option selected={String($locale).toUpperCase() === l.toUpperCase()} value={l}>{l.toUpperCase()}</option>
-		{/each}
-	</select>
-	<nav class="w-full flex justify-between items-baseline p-4 sticky top-0 border-b border-white text-white bg-gray-900">
-		<a href="/">
-			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Babel Revolution</span>
-		</a>
-		<div class="float-right flex gap-4 underline-offset-2">
-			<a href="/sessions" class="hover:underline">{$t('sessions')}</a>
-			<a href="/admin/sessions/create" class="hover:underline">{$t('createSession')}</a>
-			<a href="/admin/scenario/create" class="hover:underline">{$t('createScenario')}</a>
+<div class="h-lvh overflow-auto bg-black dark:bg-black">
+	<Toaster />
+	{#if $isLoading}
+		<p>Loading...</p>
+	{:else}
+		<select bind:value={$locale} class="fixed bottom-0 right-0 rounded-tl-xl bg-gray-900 p-4 text-white">
+			{#each $locales as l, i}
+				<option selected={String($locale).toUpperCase() === l.toUpperCase()} value={l}>{l}</option>
+			{/each}
+		</select>
+		<nav
+			class="w-full flex justify-between z-50 items-baseline p-4 sticky top-0 border-b border-white text-white bg-gray-900"
+		>
+			<a href="/">
+				<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Babel Revolution</span
+				>
+			</a>
+			<div class="float-right flex gap-4 underline-offset-2">
+				<a href="/sessions" class="hover:underline">{$t('sessions')}</a>
+				<a href="/admin/sessions/create" class="hover:underline">{$t('createSession')}</a>
+				<a href="/admin/scenario/create" class="hover:underline">{$t('createScenario')}</a>
+			</div>
+		</nav>
+		<div>
+			<slot />
 		</div>
-	</nav>
-	<slot />
-{/if}
+	{/if}
+</div>
