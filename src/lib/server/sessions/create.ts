50de9e49cf45d27abf9ecbbba928d24b1132b5a2
mathis <mathisjung02@gmail.com>
2024-09-26 07:42:41 +0200

    handle pocketbase with cookies

--- src/lib/server/sessions/create.ts
+++ src/lib/server/sessions/create.ts
@@ -1,25 +1,31 @@
-import { pb } from "$lib/pocketbase";
+import type { MyPocketBase } from '$types/pocketBase';
+import type { Scenario } from '$types/tableTypes';
 
-export async function getScenario(scenarioId: string) {
-    return await pb.collection('scenario').getOne(scenarioId);
+export async function getScenario(pb: MyPocketBase, scenarioId: string) {
+	return await pb.collection('Scenario').getOne(scenarioId);
 }
 
-export async function createSession(name: FormDataEntryValue, scenarioId: string, author: FormDataEntryValue) {
-    const sessions = await pb.collection('session').getFullList({ fields: 'id' });
-    return await pb.collection('session').create({
-        name,
-        scenario: scenarioId,
-        author,
-        slug: sessions.length + 1
-    });
+export async function createSession(
+	pb: MyPocketBase,
+	name: FormDataEntryValue,
+	scenarioId: string,
+	author: FormDataEntryValue
+) {
+	const sessions = await pb.collection('Session').getFullList({ fields: 'id' });
+	return await pb.collection('Session').create({
+		name,
+		scenario: scenarioId,
+		author,
+		slug: sessions.length + 1
+	});
 }
 
-export async function createStartNode(scenario: any, sessionId: string) {
-    return await pb.collection('node').create({
-        title: scenario.firstNodeTitle,
-        text: scenario.firstNodeText,
-        author: scenario.firstNodeAuthor,
-        session: sessionId,
-        type: 'startNode'
-    });
+export async function createStartNode(pb: MyPocketBase, scenario: Scenario, sessionId: string) {
+	return await pb.collection('Node').create({
+		title: scenario.firstNodeTitle,
+		text: scenario.firstNodeText,
+		author: scenario.firstNodeAuthor,
+		session: sessionId,
+		type: 'startNode'
+	});
 }
