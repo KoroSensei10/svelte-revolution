7966846a25aff6afc3a32b391c811654e98b0e07
Mathis <mathisjung02@gmail.com>
2024-10-14 22:18:19 +0200

    fix

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -105,7 +105,7 @@
 							{$t(homeStore.selectedNode?.title ?? 'oupsi')}
 						</div>
 						<div class="">{$t(homeStore.selectedNode?.text ?? 'oupsi')}</div>
-						{#if homeStore.selectedNode?.id === '3'}
+						{#if homeStore.selectedNode?.id === 3}
 							<form class="flex justify-between mt-2" onsubmit={submitUserMessage}>
 								<input
 									type="text"
@@ -115,7 +115,7 @@
 								/>
 								<button type="submit" class=" btn btn-accent">{$t('home.send')}</button>
 							</form>
-						{:else if homeStore.selectedNode?.id === '5'}
+						{:else if homeStore.selectedNode?.id === 5}
 							<div class="mt-2 chat chat-end">
 								<div class="rounded-full chat-image avatar ring-primary-500 ring-1">
 									<div class="w-10 rounded-full">
